6150d00c74fab5a0ef501b5698d9ad0bf67c9e18
chrislerus <lerus.chris@gmail.com>
2016-01-18 18:55:02 +0100

    Update test case

--- uploads/tests.py
+++ uploads/tests.py
@@ -1,11 +1,11 @@
 from django.test import TestCase
-from django.test.client import RequestFactory
+from django.test.client import Client
 from django.core.files.uploadedfile import SimpleUploadedFile
 
 
 class UploadTests(TestCase):
     def setUp(self):
-        self.factory = RequestFactory()
+        self.factory = Client()
         pass
 
     def test_basic_upload(self):
@@ -13,6 +13,11 @@ class UploadTests(TestCase):
         Check that the POST request to upload returns
         the uploaded photo as the response body
         """
-        response = self.factory.post("/display/", {})
+        with open('uploads/test_files/test.txt') as fp:
+            content_file = fp.read()
+            fp.close()
+        with open('uploads/test_files/test.txt') as fp:
+            response = self.factory.post("/display/", {"pic": fp})
+        content_response = response.content
 
-        self.assertEqual("test", "test")
+        self.assertEqual(content_file, content_response)
